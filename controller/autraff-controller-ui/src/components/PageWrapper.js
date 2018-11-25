1308fb24f7c90f9661820d0c58114213e1356923
nightism <sun.mingyang@gmx.com>
2018-11-25 16:00:22 +0800

    client management page

--- controller/autraff-controller-ui/src/components/PageWrapper.js
+++ controller/autraff-controller-ui/src/components/PageWrapper.js
@@ -10,50 +10,56 @@ const { Header, Footer } = Layout;
 
 const PageWrapper = () => {
   return (
-    <Layout style={{height:"100vh"}}>
-      <Header className="header">
-        {/* <div className="logo" /> */}
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={['1']}
-          style={{ lineHeight: '64px' }}
-        >
-          <Menu.Item key="1">
-            <span>Homeeee</span>
-            {/* <Link to="/" /> */}
-          </Menu.Item>
+    <Router>
+      <Layout style={{height:"100vh"}}>
+        <Header className="header">
+          {/* <div className="logo" /> */}
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['1']}
+            style={{ lineHeight: '64px' }}
+          >
+            <Menu.Item key="1">
+              <span>Home</span>
+              <Link to="/" />
+            </Menu.Item>
 
-          <Menu.Item key="2">
-            <span>Client Management</span>
-            <Link to="/client-management" />
-          </Menu.Item>
+            <Menu.Item key="2">
+              <span>Client Management</span>
+              <Link to="/client-management" />
+            </Menu.Item>
 
-          <Menu.Item key="3">
-            <span>Task Management</span>
-            {/* <Link to="/task-management" /> */}
-          </Menu.Item>
+            <Menu.Item key="3">
+              <span>Task Management</span>
+              <Link to="/task-management" />
+            </Menu.Item>
 
-          <Menu.Item key="4">
-            <span>Database Management</span>
-            {/* <Link to="/database-management" /> */}
-          </Menu.Item>
+            <Menu.Item key="4">
+              <span>Database Management</span>
+              <Link to="/database-management" />
+            </Menu.Item>
 
-          <Menu.Item key="5">
-            <span>Upload</span>
-            {/* <Link to="/uoload" /> */}
-          </Menu.Item>
+            <Menu.Item key="5">
+              <span>Upload</span>
+              <Link to="/uoload" />
+            </Menu.Item>
 
-        </Menu>
-      </Header>
+          </Menu>
+        </Header>
 
-      <ClientManagement />
 
-      <Footer style={{ textAlign: 'center' }}>
-        {/* Ant Design ©2018 Created by Ant UED */}
-        Autraff ©2018
-      </Footer>
-    </Layout>
+        <Route exact path='/client-management' component={ClientManagement} />
+
+        <Footer style={{ textAlign: 'center' }}>
+          {/* Ant Design ©2018 Created by Ant UED */}
+          Autraff ©2018
+        </Footer>
+      </Layout>
+    
+    
+    </Router>
+    
   );
 }
 
