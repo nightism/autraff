131b7be4ddd9fdbf6d7a07ead08b8f50e0c115e5
nightism <sun.mingyang@gmx.com>
2018-11-29 00:14:29 +0800

    minor changes

--- controller/autraff-controller-ui/src/index.js
+++ controller/autraff-controller-ui/src/index.js
@@ -7,6 +7,10 @@ import PageWrapper from './components/PageWrapper'
 
 ReactDOM.render(<PageWrapper />, document.getElementById('root'));
 
+// import App from './App'
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
