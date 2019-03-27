b1567889645f2038aa6e79152b48288e628a86e6
nightism <sun.mingyang@gmx.com>
2019-03-28 00:17:10 +0800

    finish up Page class

--- client/lib/Page.py
+++ client/lib/Page.py
@@ -1,22 +1,44 @@
 from util.util_human_web_browsing.content_smilarity import calculate_content_similarity
 from util.util_human_web_browsing.theme_similarity import calculate_theme_similarity
-from util.util_human_web_browsing.calculate_page_interest import calculate_interest_in_theme
+from util.util_human_web_browsing.theme_interest import calculate_interest_in_theme
+from util.util_human_web_browsing.page_interest import calculate_page_interest
+from util.util_human_web_browsing.page_staying_time import calculate_page_staying_time
 
 
 class Page:
-    def __init__(self, title, content, theme_staying_time):
+    max_content_length = 0
+
+    def __init__(self, title, content, theme_staying_time, prev_page):
         self.title = title
         self.content = content
-        self.theme_staying_time = theme_staying_time  # the time spent on current theme
-        self.intertest_in_theme = calculate_interest_in_theme(theme_staying_time)
+
+        # the time spent on current theme
+        self.theme_staying_time = theme_staying_time
+
+        # calculate the interest in current theme
+        self.interest_in_theme = calculate_interest_in_theme(self.theme_staying_time)
+
+        # calculate the content closeness with previous page
+        if prev_page is None:
+            # at the beginning of browsing
+            self.content_closeness = 0
+        else:
+            self.content_closeness = calculate_content_similarity(self.content, prev_page.content)
+
+        # record the max length of web page
+        if Page.max_content_length < len(self.content):
+            Page.max_content_length = len(self.content)
+
+        # calculate the page interest
+        self.interest_in_page = calculate_page_interest(self)
+
+        # calculate staying time for current page
+        self.staying_time = calculate_page_staying_time(self.interest_in_page,
+                                                        self.interest_in_theme,
+                                                        self.content_closeness)
 
     def calculate_theme_closeness(self, another_title):
         result = calculate_theme_similarity(self.title, another_title)
-        return result
-
-    def calculate_content_closeness(self, another_content):
-        # TODO
-        result = calculate_content_similarity(self.content, another_content)
         return result
 
     # TODO def calculate_staying_time(self, ):
---
+++ client/util/util_human_web_browsing/page_interest.py
@@ -0,0 +1,13 @@
+from lib.Page import Page
+
+
+def calculate_page_interest(page):
+    max_length = Page.max_content_length
+    page_length = len(page.content)
+
+    original_interest = (page_length * 1.0) / (max_length * 1.0)
+    if page.theme_staying_time == 0:
+        return original_interest
+    else:
+        actual_interest = original_interest * (1.0 - page.content_closeness)
+        return actual_interest
