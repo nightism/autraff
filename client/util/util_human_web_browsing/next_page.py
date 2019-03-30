44be1ae66c001220f18f22b370caa7563f8d06ec
nightism <sun.mingyang@gmx.com>
2019-03-30 16:40:03 +0800

    updates

--- client/util/util_human_web_browsing/next_page.py
+++ client/util/util_human_web_browsing/next_page.py
@@ -49,22 +49,24 @@ def calculate_link_visibility_closeness_in_a_page(page, link):
 
 def calculate_link_possibility(page, all_links):
     possibility = []
+    total = 0.0
 
     for index, link in enumerate(all_links):
         try:
             visual_effect = PHI_OF_VISUAL_EFFECT * calculate_link_visibility_closeness_in_a_page(page, link)
             theme_closeness = calculate_link_theme_closeness_in_a_page(page, link)
-            theme_interest = page.interest_in_theme
+            theme_interest = page.interest_in_them
             theme_effect = PHI_OF_CONTENT_EFFECT \
                            * (2 * theme_closeness * theme_interest / (theme_closeness ** 2 + theme_interest ** 2))
 
             effect = (theme_effect + visual_effect)
 
-            if effect > 0.0000000001:
+            if effect > 0.0001:
                 possibility.append({
                     'link': link,
                     'possibility': effect
                 })
+                total += effect
         except Exception as e:
             continue
 
@@ -73,4 +75,24 @@ def calculate_link_possibility(page, all_links):
 
     possibility.sort(key=take_possibility, reverse=True)
 
-    return possibility
+    return possibility, total
+
+
+def normalize_link_possibility(all_links, total_score):
+    num = len(all_links)
+    total_score = total_score * num
+    all_links = list(map(lambda link: link['possibility'] * num / total_score, all_links))
+
+    return all_links
+
+
+def find_link_in_distribution(num, all_links):
+    cumulative_score = 0
+    prev_link = None
+    for link in all_links:
+        if cumulative_score > num:
+            return prev_link
+        else:
+            cumulative_score + link['possibility']
+            prev_link = link
+    return prev_link
